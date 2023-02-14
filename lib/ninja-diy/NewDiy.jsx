10439d3d47bdbc944e213d6b3897349faa2bd442
Sourav <Sourav@gmail.com>
2023-02-14 18:55:43 +0530

    new diy updates

--- lib/ninja-diy/NewDiy.jsx
+++ lib/ninja-diy/NewDiy.jsx
@@ -2,13 +2,65 @@ import React, { useState } from 'react'
 import styles from '/styles/NewDiy.module.scss';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import Select from "react-select";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRegular, faMagnifyingGlass, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
+import Multiselect from 'multiselect-react-dropdown';
+import { DisabledByDefault } from '@mui/icons-material';
+
+// this.state = {
+//     options: [{ name: 'Option 1️⃣', id: 1 }, { name: 'Option 2️⃣', id: 2 }]
+// };
+
+
+
+// onSelect(selectedList, selectedItem) {
+
+// }
+
+// onRemove(selectedList, removedItem) {
+
+// }
 
 const NewDiy = () => {
 
     const [startDate, setStartDate] = useState(new Date());
+    const [selectedOptions, setSelectedOptions] = useState();
+
+    const [isShown, setIsShown] = useState(false);
+
+    const handleClick = event => {
+        // 👇️ toggle shown state
+        setIsShown(current => !current);
+
+        // 👇️ or simply set it to true
+        // setIsShown(true);
+    };
+
+    const optionList = [
+        { value: "red", label: "Paneer Butter Masala" },
+        { value: "green", label: "Palak Paneer" },
+        { value: "yellow", label: "Kadai Paneer" },
+        { value: "blue", label: "Chicken tikka" },
+        { value: "white", label: "Chicken Punjabi" }
+    ];
+
+    function handleSelect(data) {
+        setSelectedOptions(data);
+    }
 
     return (
         <div className={styles.diyMainContainer}>
+            {/* <div className={styles.diyAnimationScreen}>
+                <div className={styles.whiteBg}>
+                    <img id={styles.logo} src='CaterNinja (1).png' />
+                    <p>Get Instant Quote In <span>Four</span> Easy Steps</p>
+                    <img id={styles.ninja} src='/diy images/Group 266 (1).png' />
+                </div>
+                <div className={styles.redBg} />
+                <div className={styles.yellowBg} />
+            </div> */}
             <div className={styles.header}>
                 <img src='CaterNinja (1).png' />
                 <h4>Get Instant Quote In Four Easy Steps</h4>
@@ -116,15 +168,317 @@ const NewDiy = () => {
             <div className={styles.createYourMenuContainer}>
                 <h3>Create Your Menu</h3>
                 <hr />
-                <div className={styles.startersContainer}>
-                    <div>
-                        <label className={styles.switch}>
-                            <input type="checkbox" />
-                            <span className={styles.slider}></span>
-                        </label>
+                <div>
+                    <div className={styles.startersContainer}>
+                        <div>
+                            <label className={styles.switch}>
+                                <input type="checkbox" />
+                                <span className={styles.slider}></span>
+                            </label>
+                        </div>
+                        <div>
+                            <h6>STARTERS -</h6>
+                        </div>
+                    </div>
+                    {/* <div onClick={openForm} className={styles.starterSearchBtn} id="srchbr">
+                        <p><FontAwesomeIcon icon={faMagnifyingGlass} />  Select Starter</p>
+                        <span><FontAwesomeIcon icon={faAngleDown} />  Click here to select</span>
+                    </div> */}
+                    {/* { isShown && (<div className={styles.starterMenuContainer}>
+                        <Select
+                            className={styles.selectDropdown}
+                            options={optionList}
+                            placeholder="Select Starter"
+                            value={selectedOptions}
+                            onChange={handleSelect}
+                            isSearchable={true}
+                            isMulti
+                        />
+                        <div><button onClick={handleClick}>close</button></div>
+                    </div>)} */}
+                    <div className={styles.starterMenuContainer}>
+                        <Multiselect
+                            id="starterOptions"
+                            placeholder="Select Starter"
+                            displayValue="key"
+                            onKeyPressFn={function noRefCheck() { }}
+                            onRemove={function noRefCheck() { }}
+                            onSearch={function noRefCheck() { }}
+                            onSelect={function noRefCheck() { }}
+                            options={[
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Palak Paneer'
+                                },
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Butter Paneer'
+                                },
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Muter Paneer'
+                                },
+                                {
+                                    cat: 'Group 2',
+                                    key: 'Chilie Paneer'
+                                }
+                            ]}
+                            showCheckbox
+                        />
+                    </div>
+                    <div className={styles.starterBtmLine}>
+                        <hr />
+                    </div>
+                    <div className={styles.addMoreBtn}>
+                        <button>+ Add More</button>
+                    </div>
+                    <div className={styles.startersContainer}>
+                        <div>
+                            <label className={styles.switch}>
+                                <input type="checkbox" />
+                                <span className={styles.slider}></span>
+                            </label>
+                        </div>
+                        <div>
+                            <h6>MAINS -</h6>
+                        </div>
                     </div>
                     <div>
-                    <h6>STARTERS -</h6>
+                        <Multiselect
+                            id="starterOptions"
+                            placeholder="Select Mains"
+                            displayValue="key"
+                            onKeyPressFn={function noRefCheck() { }}
+                            onRemove={function noRefCheck() { }}
+                            onSearch={function noRefCheck() { }}
+                            onSelect={function noRefCheck() { }}
+                            options={[
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Palak Paneer'
+                                },
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Butter Paneer'
+                                },
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Muter Paneer'
+                                },
+                                {
+                                    cat: 'Group 2',
+                                    key: 'Chilie Paneer'
+                                }
+                            ]}
+                            showCheckbox
+                        />
+                    </div>
+                    <div className={styles.starterBtmLine}>
+                        <hr />
+                    </div>
+                    <div className={styles.addMoreBtn}>
+                        <button>+ Add More</button>
+                    </div>
+                    <div className={styles.startersContainer}>
+                        <div>
+                            <label className={styles.switch}>
+                                <input type="checkbox" />
+                                <span className={styles.slider}></span>
+                            </label>
+                        </div>
+                        <div>
+                            <h6>BREAD / RICE -</h6>
+                        </div>
+                    </div>
+                    <div>
+                        <Multiselect
+                            id="starterOptions"
+                            placeholder="Select Bread / Rice"
+                            displayValue="key"
+                            onKeyPressFn={function noRefCheck() { }}
+                            onRemove={function noRefCheck() { }}
+                            onSearch={function noRefCheck() { }}
+                            onSelect={function noRefCheck() { }}
+                            options={[
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Palak Paneer'
+                                },
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Butter Paneer'
+                                },
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Muter Paneer'
+                                },
+                                {
+                                    cat: 'Group 2',
+                                    key: 'Chilie Paneer'
+                                }
+                            ]}
+                            showCheckbox
+                        />
+                    </div>
+                    <div className={styles.starterBtmLine}>
+                        <hr />
+                    </div>
+                    <div className={styles.addMoreBtn}>
+                        <button>+ Add More</button>
+                    </div>
+                    <div className={styles.startersContainer}>
+                        <div>
+                            <label className={styles.switch}>
+                                <input type="checkbox" />
+                                <span className={styles.slider}></span>
+                            </label>
+                        </div>
+                        <div>
+                            <h6>DESSERT -</h6>
+                        </div>
+                    </div>
+                    <div>
+                        <Multiselect
+                            id="starterOptions"
+                            placeholder="Select Dessert"
+                            displayValue="key"
+                            onKeyPressFn={function noRefCheck() { }}
+                            onRemove={function noRefCheck() { }}
+                            onSearch={function noRefCheck() { }}
+                            onSelect={function noRefCheck() { }}
+                            options={[
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Palak Paneer'
+                                },
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Butter Paneer'
+                                },
+                                {
+                                    cat: 'Group 1',
+                                    key: 'Muter Paneer'
+                                },
+                                {
+                                    cat: 'Group 2',
+                                    key: 'Chilie Paneer'
+                                }
+                            ]}
+                            showCheckbox
+                        />
+                    </div>
+                    <div className={styles.starterBtmLine}>
+                        <hr />
+                    </div>
+                    <div className={styles.addMoreBtn}>
+                        <button>+ Add More</button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.checkPriceBtn}>
+                <button>Check Price</button>
+            </div>
+            <div className={styles.priceDetailsSection}>
+                <h3>Review Your Menu</h3>
+                <hr />
+                <div className={styles.reviewBtn}>
+                    <button>Birthday Party</button>
+                    <button>Total Guests - 50</button>
+                    <button>Veg Only</button>
+                </div>
+                <div>
+                    <div className={styles.menuName}>
+                        <div>
+                            <h6>Starter -</h6>
+                        </div>
+                        <div><p>Qt. -</p></div>
+                    </div>
+                    <div className={styles.itemName}>
+                        <div>
+                            <h6>Paneer Butter Masala</h6>
+                            <h6>Palak Paneer</h6>
+                        </div>
+                        <div>
+                            <h6>2.0 kg</h6>
+                            <h6>2.0 kg</h6>
+                        </div>
+                    </div>
+                    <div className={styles.menuName}>
+                        <div>
+                            <h6>Mains -</h6>
+                        </div>
+                    </div>
+                    <div className={styles.itemName}>
+                        <div className='mt-2'>
+                            <h6>Paneer Butter Masala</h6>
+                            <h6>Palak Paneer</h6>
+                        </div>
+                        <div>
+                            <h6>2.0 kg</h6>
+                            <h6>2.0 kg</h6>
+                        </div>
+                    </div>
+                    <div className={styles.menuName}>
+                        <div>
+                            <h6>Bread / Rice -</h6>
+                        </div>
+                    </div>
+                    <div className={styles.itemName}>
+                        <div className='mt-2'>
+                            <h6>Paneer Butter Masala</h6>
+                            <h6>Palak Paneer</h6>
+                        </div>
+                        <div>
+                            <h6>2.0 kg</h6>
+                            <h6>2.0 kg</h6>
+                        </div>
+                    </div>
+                    <div className={styles.menuName}>
+                        <div>
+                            <h6>Desserts</h6>
+                        </div>
+                    </div>
+                    <div className={styles.itemName}>
+                        <div className='mt-2'>
+                            <h6>Paneer Butter Masala</h6>
+                            <h6>Palak Paneer</h6>
+                        </div>
+                        <div>
+                            <h6>2.0 kg</h6>
+                            <h6>2.0 kg</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr id={styles.pricebtmHr} />
+            <div className={styles.backToMenu}>
+                <button>Back To Menu</button>
+            </div>
+            <div className={styles.finalPriceSection}>
+                <div className="d-flex justify-content-between">
+                    <select className="form-select" aria-label="Default select example">
+                        <option value="1">NinjaBox Delivery (Free)</option>
+                        <option value="2">Buffet setup + 1 waiter</option>
+                    </select>
+                    <p>₹ 0000</p>
+                </div>
+                <div className={styles.couponSection}>
+                    <div>
+                        <h5>Discount Coupon</h5>
+                    </div>
+                    <div className={styles.coupon}>
+                        <input />
+                        <button>Apply</button>
+                    </div>
+                </div>
+                <div className={styles.itemtotalSectn}>
+                    <div>
+                        <h6>Item Total</h6>
+                        <h6>Delivery Charges (As Per Actual)</h6>
+                    </div>
+                    <div>
+                        <p>₹ 0000</p>
+                        <p>₹ 0000</p>
                     </div>
                 </div>
             </div>
