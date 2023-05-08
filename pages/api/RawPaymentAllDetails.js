2d7a3ce00db724933780f63b2e5948e7894e7011
manojs7 <takmanoj369@gmail.com>
2023-05-08 14:13:39 +0530

    build ran with env and mongo atlas

--- pages/api/RawPaymentAllDetails.js
+++ pages/api/RawPaymentAllDetails.js
@@ -1,14 +1,14 @@
 import clientPromise from "../../lib/mongodb";
 
-export default async function handler(req, res) {
-  const client = await clientPromise;
+export default function handler(req, res) {
+  const client = clientPromise;
   const db = client.db("CaterNinja");
   if (req.method === "POST") {
     let bodyObject = (req.body);
-    let myPost = await db.collection("RawPaymentAllDetails").insertOne(bodyObject);
+    let myPost = db.collection("RawPaymentAllDetails").insertOne(bodyObject);
     res.json(myPost);
   } else {
-    const allPosts = await db.collection("RawPaymentAllDetails").find({}).toArray();
+    const allPosts = db.collection("RawPaymentAllDetails").find({}).toArray();
     res.json({ status: 200, data: allPosts });
   }
 }
