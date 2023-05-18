1317fb1b84eceab668d1e2fcca89069a65df6efa
Sourav <Sourav@gmail.com>
2023-05-18 14:36:00 +0530

    editable input, table added, error fixed

--- pages/testingPage.js
+++ pages/testingPage.js
@@ -1,44 +1,37 @@
 import React from 'react'
-import { Table } from '@nextui-org/react';
 
 const testingPage = () => {
     return (
         <div>
-            <Table
-                aria-label="Example table with static content"
-                css={{
-                    height: "auto",
-                    minWidth: "100%",
-                }}
-            >
-                <Table.Header>
-                    <Table.Column>NAME</Table.Column>
-                    <Table.Column>ROLE</Table.Column>
-                    <Table.Column>STATUS</Table.Column>
-                </Table.Header>
-                <Table.Body>
-                    <Table.Row key="1">
-                        <Table.Cell>Tony Reichert</Table.Cell>
-                        <Table.Cell>CEO</Table.Cell>
-                        <Table.Cell>Active</Table.Cell>
-                    </Table.Row>
-                    <Table.Row key="2">
-                        <Table.Cell>Zoey Lang</Table.Cell>
-                        <Table.Cell>Technical Lead</Table.Cell>
-                        <Table.Cell>Paused</Table.Cell>
-                    </Table.Row>
-                    <Table.Row key="3">
-                        <Table.Cell>Jane Fisher</Table.Cell>
-                        <Table.Cell>Senior Developer</Table.Cell>
-                        <Table.Cell>Active</Table.Cell>
-                    </Table.Row>
-                    <Table.Row key="4">
-                        <Table.Cell>William Howard</Table.Cell>
-                        <Table.Cell>Community Manager</Table.Cell>
-                        <Table.Cell>Vacation</Table.Cell>
-                    </Table.Row>
-                </Table.Body>
-            </Table>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
